89bdae578a64faaba6e312c50a78076d4c2db611
sebastian8080 <sebas25211@hotmail.com>
2026-03-23 15:50:02 -0500

    feat: add relation on comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,11 +12,29 @@ class Comment extends Model
     protected $primaryKey = 'comment_id';
 
     protected $fillable = [
-        'comment_id', 'listing_id', 'user_id', 'property_code', 
-        'type', 'value', //nuevas variables para que los comentarios sean generales y no solo para notificaciones  
+        'comment_id',
+        'listing_id',
+        'user_id',
+        'property_code',
+        'type',
+        'value', //nuevas variables para que los comentarios sean generales y no solo para notificaciones  
         'comment',
-        'property_price_prev', 'property_price', 'property_price_min_prev', 'property_price_min',
-        'viewed', 'created_at', 'updated_at'
+        'property_price_prev',
+        'property_price',
+        'property_price_min_prev',
+        'property_price_min',
+        'viewed',
+        'created_at',
+        'updated_at'
     ];
 
+    public function listing()
+    {
+        return $this->belongsTo(\App\Models\Listing::class, 'listing_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'user_id');
+    }
 }
