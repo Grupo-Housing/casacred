8361e2b1f604f0a11382e689724664c9b16aca58
sebastian8080 <sebas25211@hotmail.com>
2026-03-26 16:18:54 -0500

    feat: comment commando to assign contact queue daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:assign-contacts')->dailyAt('07:00');
+        //$schedule->command('queue:assign-contacts')->dailyAt('07:00');
     }
 
     /**
